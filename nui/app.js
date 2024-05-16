e55d1f1b336ed24e9049d3f72719fafe6e4a4ba8
Drewno1 <drewno.dev@gmail.com>
2024-05-16 23:28:36 +0200

    Simple bug fixes

--- nui/app.js
+++ nui/app.js
@@ -1,20 +1,22 @@
 const compass = document.querySelector(".compass");
 
-var heading = 120;
+var heading;
+
+window.addEventListener("message", (e) => {
+  const data = e.data;
+
+  if (data.type === "updateHeading") {
+    heading = data.heading;
+    updateRot();
+    console.log(`calc(${heading * -1 * 10 - 2700}px - 50px) 0`);
+  }
+});
 
 function updateRot() {
   compass.style.backgroundPosition = `calc(${heading * -1 * 10}px - 50px) 0`;
-}
-
-console.log(`calc(${heading * -1 * 10 - 2700}px - 50px) 0`);
-
-function test() {
-  heading = Math.floor(Math.random() * (360 - 0 + 1) + 0);
-  //   updateRot(heading);
 }
 
 // updateRot();
 
 // updateRot(180);
 // setInterval(test, 1000);
-setInterval(updateRot, 0.1);
